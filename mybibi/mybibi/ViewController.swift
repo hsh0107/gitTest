eaedd119cbe7c4e9c1051fa3f66cb20b47c4af22
hsh <1161694014@qq.com>
2015-10-24 16:07:48 +0800

    add something

--- mybibi/mybibi/ViewController.swift
+++ mybibi/mybibi/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       println("hsh is cool")
     }
 
     override func didReceiveMemoryWarning() {
