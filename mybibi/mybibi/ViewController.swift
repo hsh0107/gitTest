bf17b87fd1d6cec7a74ae82380a867a9dc76ff1c
hsh <1161694014@qq.com>
2015-10-24 15:58:45 +0800

    init

---
+++ mybibi/mybibi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mybibi
+//
+//  Created by hsh on 10/24/15.
+//  Copyright (c) 2015 hsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
